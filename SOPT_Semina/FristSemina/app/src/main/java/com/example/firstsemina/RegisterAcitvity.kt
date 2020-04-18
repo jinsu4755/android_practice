6fbdeb367dadad69b2a0ae813a82ca1921c06160
jinsu4755 <parkjinsu4755@daum.net>
2020-04-18 16:59:15 +0900

    first semina finish

--- SOPT_Semina/FristSemina/app/src/main/java/com/example/firstsemina/RegisterAcitvity.kt
+++ SOPT_Semina/FristSemina/app/src/main/java/com/example/firstsemina/RegisterAcitvity.kt
@@ -2,11 +2,16 @@ package com.example.firstsemina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterAcitvity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btn_register.setOnClickListener {
+            finish()
+        }
     }
 }

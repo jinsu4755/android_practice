2402852b24c6d50c29eab81c079d638fecbe2530
jinsu4755 <parkjinsu4755@daum.net>
2020-05-11 17:56:49 +0900

    [SOPT] 3차 세미나 필수과제 2

---
+++ SOPT_SeminaTask/Semina_3rd_Task2/app/src/main/java/com/antilog/semina_3rd_task2/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.antilog.semina_3rd_task2.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.antilog.semina_3rd_task2.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- SOPT_SeminaTask/Semina_3rd_Task2/app/src/main/java/com/antilog/semina_3rd_task2/ui/RegisterActivity.kt
+++ SOPT_SeminaTask/Semina_3rd_Task2/app/src/main/java/com/antilog/semina_3rd_task2/ui/RegisterActivity.kt
@@ -1,12 +1,13 @@
-package com.antilog.semina_3rd_task2
+package com.antilog.semina_3rd_task2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.antilog.semina_3rd_task2.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }

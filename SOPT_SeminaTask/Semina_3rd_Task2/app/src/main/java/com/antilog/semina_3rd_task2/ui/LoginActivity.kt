433551bf0ad540fe8fe8a861fda9a18d35aba6c7
jinsu4755 <parkjinsu4755@daum.net>
2020-05-12 13:38:29 +0900

    [SOPT] 3차 세미나 필수과제 2

--- SOPT_SeminaTask/Semina_3rd_Task2/app/src/main/java/com/antilog/semina_3rd_task2/ui/LoginActivity.kt
+++ SOPT_SeminaTask/Semina_3rd_Task2/app/src/main/java/com/antilog/semina_3rd_task2/ui/LoginActivity.kt
@@ -1,13 +1,21 @@
 package com.antilog.semina_3rd_task2.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.antilog.semina_3rd_task2.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_login.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }

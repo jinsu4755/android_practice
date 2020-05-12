e8ef40ea74b4ff62c4a08503d6f5629afcf71db5
jinsu4755 <parkjinsu4755@daum.net>
2020-05-13 03:23:37 +0900

    [SOPT] ( #2 ) 3차 세미나: status bar 반투명화

--- SOPT_SeminaTask/Semina_3rd_Task2/app/src/main/java/com/antilog/semina_3rd_task2/ui/MainActivity.kt
+++ SOPT_SeminaTask/Semina_3rd_Task2/app/src/main/java/com/antilog/semina_3rd_task2/ui/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.antilog.semina_3rd_task2.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.antilog.semina_3rd_task2.R
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init(){
+        //App Compact Toolbar 인스턴스 얻기
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        // Status Bar 높이만큼 Padding 부여
+        toolbar.setPadding(0, getStatusBarHeight(), 0, 0);
+
+    }
+
+    //status bar 높이 계산
+    private fun getStatusBarHeight(): Int {
+        var result = 0
+        var resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            result = resources.getDimensionPixelSize(resourceId)
+        }
+        return result
     }
 }

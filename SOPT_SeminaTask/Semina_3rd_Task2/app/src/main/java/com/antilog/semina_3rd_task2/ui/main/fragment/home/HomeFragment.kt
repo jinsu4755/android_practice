225f61f3fa7f90d068bf30840f18ea58d8c527b8
jinsu4755 <parkjinsu4755@daum.net>
2020-05-17 01:51:32 +0900

    [SOPT] ( #2 ) 3차 세미나: Home화면 수정

--- SOPT_SeminaTask/Semina_3rd_Task2/app/src/main/java/com/antilog/semina_3rd_task2/ui/main/fragment/home/HomeFragment.kt
+++ SOPT_SeminaTask/Semina_3rd_Task2/app/src/main/java/com/antilog/semina_3rd_task2/ui/main/fragment/home/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.DividerItemDecoration
 
 import com.antilog.semina_3rd_task2.R
 import com.antilog.semina_3rd_task2.data.home_fragment.HomeData
@@ -28,6 +30,10 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val decoration = DividerItemDecoration(context, LinearLayout.VERTICAL)
+        rv_home.addItemDecoration(decoration)
+
         homeAdapter = HomeAdapter(view.context)
         rv_home.adapter = homeAdapter
         loadDatas()
@@ -39,22 +45,63 @@ class HomeFragment : Fragment() {
                 HomeData(
                     userName = "Park",
                     img_profile = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fd44x6A%2Fbtqzu7dKbZ6%2FlOoGM6j0rjWD3p8kKfB8ck%2Fimg.jpg",
-                    img_contents = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FcdiIf5%2FbtqDsaeCxMV%2FbeY1EOcnnVBaXHVqyXOC5K%2Fimg.png"
+                    img_contents = "https://avatars0.githubusercontent.com/u/45380072?s=460&u=46ee2e7073f0c5a0232a7420e769c683ed6ff61b&v=4"
                 )
             )
             add(
                 HomeData(
                     userName = "Jin",
-
                     img_profile = "https://avatars0.githubusercontent.com/u/45380072?s=460&u=46ee2e7073f0c5a0232a7420e769c683ed6ff61b&v=4",
-                    img_contents = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FcdiIf5%2FbtqDsaeCxMV%2FbeY1EOcnnVBaXHVqyXOC5K%2Fimg.png"
+                    img_contents = "https://avatars0.githubusercontent.com/u/45380072?s=460&u=46ee2e7073f0c5a0232a7420e769c683ed6ff61b&v=4"
                 )
             )
             add(
                 HomeData(
                     userName = "Su",
                     img_profile = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fd44x6A%2Fbtqzu7dKbZ6%2FlOoGM6j0rjWD3p8kKfB8ck%2Fimg.jpg",
-                    img_contents = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FcdiIf5%2FbtqDsaeCxMV%2FbeY1EOcnnVBaXHVqyXOC5K%2Fimg.png"
+                    img_contents = "https://avatars0.githubusercontent.com/u/45380072?s=460&u=46ee2e7073f0c5a0232a7420e769c683ed6ff61b&v=4"
+                )
+            )
+            add(
+                HomeData(
+                    userName = "Park",
+                    img_profile = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fd44x6A%2Fbtqzu7dKbZ6%2FlOoGM6j0rjWD3p8kKfB8ck%2Fimg.jpg",
+                    img_contents = "https://avatars0.githubusercontent.com/u/45380072?s=460&u=46ee2e7073f0c5a0232a7420e769c683ed6ff61b&v=4"
+                )
+            )
+            add(
+                HomeData(
+                    userName = "Jin",
+                    img_profile = "https://avatars0.githubusercontent.com/u/45380072?s=460&u=46ee2e7073f0c5a0232a7420e769c683ed6ff61b&v=4",
+                    img_contents = "https://avatars0.githubusercontent.com/u/45380072?s=460&u=46ee2e7073f0c5a0232a7420e769c683ed6ff61b&v=4"
+                )
+            )
+            add(
+                HomeData(
+                    userName = "Su",
+                    img_profile = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fd44x6A%2Fbtqzu7dKbZ6%2FlOoGM6j0rjWD3p8kKfB8ck%2Fimg.jpg",
+                    img_contents = "https://avatars0.githubusercontent.com/u/45380072?s=460&u=46ee2e7073f0c5a0232a7420e769c683ed6ff61b&v=4"
+                )
+            )
+            add(
+                HomeData(
+                    userName = "Park",
+                    img_profile = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fd44x6A%2Fbtqzu7dKbZ6%2FlOoGM6j0rjWD3p8kKfB8ck%2Fimg.jpg",
+                    img_contents = "https://avatars0.githubusercontent.com/u/45380072?s=460&u=46ee2e7073f0c5a0232a7420e769c683ed6ff61b&v=4"
+                )
+            )
+            add(
+                HomeData(
+                    userName = "Jin",
+                    img_profile = "https://avatars0.githubusercontent.com/u/45380072?s=460&u=46ee2e7073f0c5a0232a7420e769c683ed6ff61b&v=4",
+                    img_contents = "https://avatars0.githubusercontent.com/u/45380072?s=460&u=46ee2e7073f0c5a0232a7420e769c683ed6ff61b&v=4"
+                )
+            )
+            add(
+                HomeData(
+                    userName = "Su",
+                    img_profile = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2Fd44x6A%2Fbtqzu7dKbZ6%2FlOoGM6j0rjWD3p8kKfB8ck%2Fimg.jpg",
+                    img_contents = "https://avatars0.githubusercontent.com/u/45380072?s=460&u=46ee2e7073f0c5a0232a7420e769c683ed6ff61b&v=4"
                 )
             )
         }
